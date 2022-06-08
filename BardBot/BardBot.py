165822caa4a58b43934951bd3f2b174759760916
wplee327 <wplee.327@gmail.com>
2022-06-08 15:39:14 -0400

    Add local webserver to BardBot and output new commands to a web page

--- BardBot/BardBot.py
+++ BardBot/BardBot.py
@@ -1,15 +1,13 @@
-from unittest import case
+import airium
 import discord
+import multiprocessing
+import subprocess
 
 bot = discord.Client()
 
 @bot.event
 async def on_ready():
-    print(f'[BardBot] Connected to Discord.')
-    queueFile = open('QueueItems.txt', 'w')
-    queueFile.write('')
-    queueFile.close()
-    
+    print('[BardBot] Connected to Discord.')
 
 @bot.event
 async def on_message(message):
@@ -20,24 +18,55 @@ async def on_message(message):
     print(f'{message.author}: {messageContents}')
 
     if messageContents.startswith('!!'):
-        commandArgs = messageContents.split(' ', 1)
-        process_command(commandArgs[0], commandArgs[1])
+        process_command(messageContents)
 
-def process_command(command, args):
-    match command:
+def process_command(command):
+    commandParts = command.split(' ', 1)
+    match commandParts[0]:
         case '!!play':
-            retries = 5
-            while retries > 0:
-                try:
-                    queueFile = open('QueueItems.txt', 'a')
-                    queueFile.write(f'{args}\n')
-                    queueFile.close()
-                    print('[BardBot] Song request added to queue.')
-                except:
-                    print(f'[BardBot] Problem encountered while adding request to queue. Retries left: {retries}')
-                    retries -= 1
-                    continue
-                break
-        
+            output_html('play', commandParts[1])
+        case '!!pause':
+            output_html('pause', '')
+        case '!!resume':
+            output_html('resume', '')
+        case '!!skip':
+            output_html('skip', '')
+        case '!!volume':
+            output_html('volume', commandParts[1])
 
-bot.run('OTgyMzUyNzI0Nzk0NDM3NjQy.GpGcuZ.euQh0O52q3p5px7aw0bstYk5VuHXrpVARY1blM')
+def output_html(command, arg):
+    doc = airium.Airium()
+    queueFile = open('CommandQueue.html', 'w+b')
+    doc('<!DOCTYPE html>')
+    with doc.html():
+        with doc.head():
+            doc.title('BardBot Command Queue')
+            doc.script(src='https://livejs.com/live.js')
+        with doc.body():
+            with doc.p(id='command'):
+                doc(command)
+            with doc.p(id='arg'):
+                doc(arg)
+    queueFile.write(bytes(doc))
+    queueFile.close()
+
+def run_local_webserver():
+    print('[BardBot] Starting local webserver.')
+    output_html('', '')
+    subprocess.call('python -m http.server --bind localhost 8888')
+
+def run_discord_bot():
+    print('[BardBot] Starting Discord connection.')
+    bot.run('OTgyMzUyNzI0Nzk0NDM3NjQy.GpGcuZ.euQh0O52q3p5px7aw0bstYk5VuHXrpVARY1blM')
+
+def run_parallel(*functions):
+    processes = []
+    for function in functions:
+        process = multiprocessing.Process(target=function)
+        process.start()
+        processes.append(process)
+    for process in processes:
+        process.join()
+
+if __name__ == '__main__':
+    run_parallel(run_local_webserver, run_discord_bot)
